263e5c0a456cc01d6870b7cb4f679028ef258a8d
Randy Eckman <emanspeaks@gmail.com>
2025-07-11 22:53:39 -0500

    updates to logging from iconbrowser example

--- pyrig/logging.py
+++ pyrig/logging.py
@@ -1,5 +1,6 @@
 from pyapp.logging import (  # noqa: F401
     get_logger, DEBUGLOW, LOGSTDOUT, LOGSTDERR, LOGTQDM, DEBUGLOW2,
     APP_LOG_LEVEL_NAMES, WARN, ERROR, DEBUG, INFO, CRITICAL, WARNING,
-    Logger, log_func_call
+    Logger, log_func_call, log_exc, log_message, log_debuglow2, log_debuglow,
+    log_debug, log_info, log_warning, log_error, log_critical,
 )
